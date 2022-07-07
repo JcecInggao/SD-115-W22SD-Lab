4ca2bb80db05aa2cdbfbe48d46c2cdb48639b40e
Jan <jancecinggao@gmail.com>
2022-07-07 05:04:05 -0500

    Inital Commi

---
+++ Lab3/Lab3/Models/Hotel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReview
+{
+    class Hotel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Client> Clients { get; set; }
+        public List<Room> Rooms { get; set; }
+        public List<Reservation> Reservations { get; set; }
+
+        public Hotel(string name, string address)
+        {
+            Name = name;
+            Address = address;
+            Clients = new List<Client>();
+            Rooms = new List<Room>();
+            Reservations = new List<Reservation>();
+        }
+    }
+}
---
+++ Lab3/Lab3/Models/Reservation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReview
+{
+    class Reservation
+    {
+        public int ReservationId { get; set; }
+        public DateTime Date { get; set; }
+        public int Occupants { get; set; }
+        public bool IsCurrent { get; set; }
+        public Client Client { get; set; }
+        public Room Room { get; set; }
+
+
+        // CONSTRUCTORS
+        public Reservation() { }
+        public Reservation(int occupants, Client client, Room room)
+        {
+            Date = DateTime.Now;
+            Occupants = occupants;
+            IsCurrent = true;
+            Client = client;
+            Room = room;
+        }
+    }
+}
